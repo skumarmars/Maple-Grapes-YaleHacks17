94d861206efbb85cae9a96c2d87afb7248c8308c
FaithAB <32501997+FaithAB@users.noreply.github.com>
2017-12-02 16:53:15 -0600

    Update and rename server/controllers/sample.js to server/manual.js

---
+++ server/manual.js
@@ -0,0 +1,35 @@
+const machinelearning = {  
+  "crunchbase" : 465, // https://www.crunchbase.com/search/organization.companies/74514cbc45a68d6454377395135150ad9ce7d21c i.e. this is 465
+  "googlePatent" : 3274, // https://patents.google.com/?q=machine+learning&after=priority:20141201 i.e. this is 3403 
+  "googleScholar" : 1010000 // https://scholar.google.com/scholar?q=machine+learning&hl=en&as_sdt=0%2C7&as_ylo=2012&as_yhi= i.e. this is 1010000
+}
+
+const artificialintelligence = {
+  "crunchbase" : 698, // https://www.crunchbase.com/search/organization.companies/88fe741bc6be5800e09c4abd89702ee284cf00a0e 
+  "googlePatent" : 3808, // https://patents.google.com/?q=artificial+intelligence&after=priority:20141201
+  "googleScholar" : 435000 
+}
+
+const blockchain = {
+  "crunchbase" : 694, 
+  "googlePatent" : 594, // https://patents.google.com/?q=blockchain&after=priority:20141201
+  "googleScholar" : 8840
+}
+
+const biometrics = {
+  "crunchbase" : 20, // https://www.crunchbase.com/search/organization.companies/a083dcd1e192aa8e19114a9ef43589cabe3e487b
+  "googlePatent" : 3626, // https://patents.google.com/?q=biometrics&after=priority:20141201
+  "googleScholar" : 71900
+}
+
+const internetofthings = {
+  "crunchbase" : 428, // https://www.crunchbase.com/search/organization.companies/5c4906c56aacff0407479e7c471f66476d187e4e
+  "googlePatent" : 6222, // https://patents.google.com/?q=internet+of+things&after=priority:20141201
+  "googleScholar" : 783000
+}
+
+const augmentedreality = {
+  "crunchbase" : 143, // https://www.crunchbase.com/search/organization.companies/2f965785c0046df815b71767347e74f78c6aed2f
+  "googlePatent" : 53, // https://patents.google.com/?q=augmented+reality&after=priority:20141201
+  "googleScholar" : 101000
+}
