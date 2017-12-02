8e8879836f440b280206830d58a6305e17ed569b
Shekhar Kumar <shekhar.d.kumar@gmail.com>
2017-12-02 15:04:25 -0500

    commit

--- server/controllers/getQueryScore.js
+++ server/controllers/getQueryScore.js
@@ -1,10 +1,29 @@
+const cheerio = require('cheerio');
+const axios = require('axios');
+
+
 getQueryScore = () => {
   var queryScore = 0;
   getCrunchBaseScore();
-
+  getGooglePatentScore();
+  getGoogleScholarScore();
+  getGoogleScholarScore();
 }
 
 getCrunchBaseScore = () => {
   queryScore += 1;
   console.log(queryScore);
 };
+
+getGooglePatentScore = () => {
+  const query = "machine+learning"
+  axios.get(`https://patents.google.com/?q=${query}&after=priority:20160101`)
+  .then((data) => {
+    const $ = cheerio.load(data)
+    const results = $('.c');
+    console.log(data)
+  })
+  // https://patents.google.com/?q=machine+learning&after=priority:20160101
+}
+
+module.exports = getGooglePatentScore
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const getGooglePatentScores = require('../controllers/getQueryScore');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  getGooglePatentScores()
 });
 
 router.get('/hello', (req, res) => {
