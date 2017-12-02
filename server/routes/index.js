3a4e10966c2411e985684dcd8ac19817a3d32f44
Shekhar Kumar <shekhar.d.kumar@gmail.com>
2017-12-02 17:21:35 -0500

    fix scoring

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,24 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const getGooglePatentScores = require('../controllers/getQueryScore');
+const getIndustryArray = require('../controllers/getIndustryRelevant');
 
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  getGooglePatentScores()
-});
-
-router.get('/hello', (req, res) => {
-  const peter = {
-    name: 'peter',
-    age: 20
-  }
-  getQueryScore();
-  res.json(peter)
+router.post('/industry', (req, res) => {
+  var IndustryArray = getIndustryArray(req);
 })
-router.post('/api/fname', (req, res) => {
-  const data = req.body;
-  console.log(data.query);
-  res.json('The data is posted');
-})
+
 module.exports = router;
