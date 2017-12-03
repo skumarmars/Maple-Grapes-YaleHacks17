85d2125d4d137469853cc4460fa09f79b4690e09
mxdi9i7 <peter.zheng88228@gmail.com>
2017-12-03 03:57:53 -0500

    snap shot

--- server/controllers/getIndustryArray.js
+++ server/controllers/getIndustryArray.js
@@ -1,45 +1,10 @@
-const cheerio = require('cheerio');
-const axios = require('axios');
 
-async function getUrl(industry, tech, type) {
-  const query = `http://www.google.com/search?q=${industry}+${tech}+${type}`;
-  let htmlString = await axios.get(query)
-  const $ = cheerio.load(htmlString.data);
-  const targetAnchor = $('.r');
-  const showData = targetAnchor[0].children[0].attribs.href.slice(7);
-  const url = showData.split("&sa=")[0];
-  return url;
-  console.log(url)
-};
 
-// getTitle = (url) => {
 
-//   const new_url = url.split('/').slice(-1)[0];
-//   let modified_url = new_url.split('-');
 
-//   for (var i = 0; i < modified_url.length; i ++){
-//     let word = modified_url[i]
-//     let modified_word = (word.charAt(0).toUpperCase() + word.slice(1));
-//     modified_url[i] = modified_word;
-//   }
-
-//   const final_Url = modified_url.join(" ")
-
-//   return final_Url
-
-// };
-
-getIndustryArray = (industry) => {
-
-  var IndustryArray = [];
-
-  getUrl(industry, "Machine Learning", "news").then((url) => {
-    IndustryArray.push(url);
-  }) 
-
-  console.log(IndustryArray);
-  return IndustryArray
-
+getIndustryArray = (industry, field) => {
+  
+ 
 }
 
 module.exports = getIndustryArray;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,49 @@
 var express = require('express');
 var router = express.Router();
-const getIndustryArray = require('../controllers/getIndustryArray');
-const respondData = require('..//controllers/chart');
+const respondData = require('../controllers/chart');
+const cheerio = require('cheerio');
+const axios = require('axios');
+const google = require('google');
+
 router.post('/industry', (req, res) => {
   const industryQuery = req.body.industry; 
-  var IndustryArray = getIndustryArray(industryQuery);
-  res.json(IndustryArray)
+  const fieldQuery = req.body.field;
+  var completeField;
+  switch(fieldQuery) {
+    case "ml":
+      completeField = "Machine Learning";
+      break;
+    case "ai":
+      completeField = "Artificial Intelligence";
+      break;
+    case "ar":
+      completeField = "Augmented Reality";
+      break;
+    case "bc":
+      completeField = "Blockchain";
+      break;
+    case "iot":
+      completeField = "Internet of Things";
+      break;
+    case "bio":
+      completeField = "Biometrics";
+      break;
+  }
+    const query = `${industryQuery} ${completeField} news`;
+    google(query, (err, data) => {
+        const result = {
+          title: data.links[0].title,
+          url: ""
+        }
+        for (var i = 0; i < data.links.length; i ++) {
+          var link = data.links[i];
+          if (link.href !== null) {
+            result.url = link.href
+            result.title = link.title
+          }
+        }
+        res.json(result)
+    })
 })
 
 router.get('/api/test', (req, res) => {
